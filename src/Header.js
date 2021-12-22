b676a503acd4e6fe8f293977a9fca185c0a931c0
raybeak <raymond.beak@wavebridgelabs.com>
2021-12-22 17:44:57 +0900

    add:API연결 및 초마다 업데이트하도록 설정

--- src/Header.js
+++ src/Header.js
@@ -1,34 +1,76 @@
+import React, { useEffect, useRef, useState } from 'react';
 import dogeLogo from './dogelogo.svg';
 import './Header.scss';
 import axios from "axios";
 
-
-axios.request({
-  method: 'GET',
-  url: 'https://api.upbit.com/v1/ticker',
-  params: {markets: 'KRW-DOGE'},
-  headers: {Accept: 'application/json'},
-  timeout:1000,
-  responseType:'json'
-}).then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  console.error(error);
-});
-
 function Header(){
-    return(
+    const [upbitCoinData,setUpbitCoinData]=useState(null);
+    const [apiError,setApiError]=useState(null);
+
+    const getCoinData = async()=>{
+        
+        const options = {
+            method: 'GET',
+            url: 'https://api.upbit.com/v1/ticker',
+            headers: {Accept: 'application/json'},
+            params: {markets: 'KRW-DOGE'},
+            };
+
+        await axios.request(options).then(function (response) {
+            setApiError(null);
+            setUpbitCoinData(response.data);
+        }).catch(function (e) {
+            console.log(e);
+            setApiError(e);
+        });
+
+    }
+    const interval = useRef();
+
+    useEffect(()=>{
+       interval.current=setInterval(()=>{getCoinData()},1000);
+        return ()=>{
+            clearInterval(interval.current)
+        };
+    },[])
+
+        if (apiError !== null) { 
+        return(
         <div className='header'>
             <div className='flexContainerLogo'>
               <img src={dogeLogo} className='dogeLogo' alt='dogeLogo'/>
               <div className='flexContainerCoinValue'>
-                  <h3 className='coinValueDisc'>Doge Value Now (KRW) :</h3>
-                  <h1 className='coinValue'>{upbitCoinData.trade_price}</h1>
-                  <h1 className='change' style={upbitCoinData.change==='EVEN'?{color:'white'}:upbitCoinData.change==='FALL'?{color:'red'}:{color:'green'}}>{upbitCoinData.change}</h1>
+                    <h3 className='coinValueDisc'>Doge Value Now (KRW) :</h3>
+                    <h3 className='coinValue'>Error{apiError}</h3>
               </div>
             </div>  
         </div>
     )
+        }
+        if(upbitCoinData === null){
+        return(
+        <div className='header'>
+            <div className='flexContainerLogo'>
+              <img src={dogeLogo} className='dogeLogo' alt='dogeLogo'/>
+              <div className='flexContainerCoinValue'>
+                    <h3 className='coinValueDisc'>Doge Value Now (KRW) :</h3>
+                    <h1 className='coinValue'>null</h1>
+              </div>
+            </div>  
+        </div>
+    )}
+        return(
+        <div className='header'>
+            <div className='flexContainerLogo'>
+              <img src={dogeLogo} className='dogeLogo' alt='dogeLogo'/>
+              <div className='flexContainerCoinValue'>
+                    <h3 className='coinValueDisc'>Doge Value Now (KRW) :</h3>
+                    <h1 className='coinValue'>{upbitCoinData[0].trade_price}</h1>
+                    <h1 className='change' style={upbitCoinData[0].change==='EVEN'?{color:'white'}:upbitCoinData[0].change==='FALL'?{color:'red'}:{color:'green'}}>{upbitCoinData[0].change}</h1>
+              </div>
+            </div>  
+        </div>
+    )   
 }
 
 export default Header;
