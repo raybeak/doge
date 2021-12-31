b6c3d812e1fcc63a78077019a93180cb0f6c127e
raybeak <raymond.beak@wavebridgelabs.com>
2021-12-31 14:57:10 +0900

    add: xl에서 flex가 넘어가던것 수정

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.render(
     <App 
       img='doge.svg' 
       title='도지코인은 뭔가요?' 
-      disc={`도지코인은 전 세계의 시바 이누들에게 사랑 받는 오픈소스 p2p 디지털 통화입니다.\n라이트코인 기반 포크로 2013년에 밈(Meme)문화의 파생 코인으로 만들어 졌습니다.`}
+      disc={`도지코인은 전 세계의 시바 이누들에게 사랑 받는 오픈소스 p2p 디지털 통화 입니다.\n라이트코인 기반 포크로 2013년에 밈(Meme)문화의 파생 코인으로 만들어 졌습니다.`}
       alt={false}
     />   
     {/* <App backgroundColor='#ffffff' img ='ltc.svg' title='라이트코인은 비트코인을 포크한 알트코인중 하나입니다 ' disc=''/> */}
